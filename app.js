e188c7d3300f7446501ccd41d614c966ee813a3a
annaboyatyuk <annaboyatyuk@gmail.com>
2018-02-19 22:03:13 -0800

    i dont know what im doing

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var btn = document.getElementById('btn');
 var input = document.getElementById('item');
 var tasks = document.getElementById('tasks');
 
-var allpictures = [];
+Pictures.allpictures = [];
 
 
 function Pictures(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  allpictures.push(this);
+  Pictures.allpictures.push(this);
 }
 
 
@@ -110,25 +110,25 @@ function myFunction() {
 
 
 
-// function dropdownMenu() {
-//   document.getElementById('myDropdown').classList.toggle('show');
-// }
+function dropdownMenu() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
 
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdown = document.getElementById('dropdown');
-//     var dropdowns = document.getElementsByClassName('dropdown-content');
-//     dropdown.textContent = Item.names;
-//     dropdown.appendChild(dropdowns);
-//     for (var i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// };
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdown = document.getElementById('dropdown');
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    dropdown.textContent = Pictures.allpictures.names;
+    dropdown.appendChild(dropdowns);
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
 
 
 
