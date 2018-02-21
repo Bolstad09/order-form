c6018f3010efbefb4514d84902215924e36d4e3c
Richard Tae <taerichard@yahoo.com>
2018-02-20 20:03:46 -0800

    works now

--- app.js
+++ app.js
@@ -36,6 +36,42 @@ console.log('check if it works');
 }
 bootStrapApplication();
 
+function getImageFiles(){
+  console.log('worked');
+  var cart = JSON.parse(localStorage.getItem('shoppingcart'));
+  var imgListTag = document.getElementById('cartlist');
+  for(var i = 0; i < cart.length; i++){
+    var formTag = document.createElement('form');
+    formTag.id = cart[i].name;
+    var imgTag = document.createElement('img');
+    imgTag.src = 'img/' + cart[i].name + '.jpg';
+    var deleteTag = document.createElement('input');
+    deleteTag.type='submit';
+    deleteTag.value = cart[i].name;
+    deleteTag.addEventListener('click', removeFromCart);
+    formTag.appendChild(imgTag);
+    formTag.appendChild(deleteTag);
+    imgListTag.appendChild(formTag);
+  }
+
+}
+window.onload = function(){
+    getImageFiles();
+    
+}
+
+
+
+function removeFromCart(event){
+
+  var items = JSON.parse(localStorage.getItem('shoppingcart'));
+  items = items.filter(function(item){
+      return item.name !== event.target.value;
+  });
+  localStorage.setItem('shoppingcart', JSON.stringify(items));
+  getImageFiles();
+}
+
 //need 17img
 //create function that gets local storage array JSON.parse
 //go through all 17image objects and get the ones that have the same name as your objects in local storage
