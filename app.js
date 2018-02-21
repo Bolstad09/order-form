1d0e8f0f438b47234e06c5ca84979511a342a7f3
Richard Tae <taerichard@yahoo.com>
2018-02-20 17:22:56 -0800

    local storage works now yay

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 'use strict';
 
-
-function retrieveCart(event){
-  event.target.name === 'count'
-}
-
-
+//local storage
 function addToCart(event){
-var jsonCart = localStorage.getItem('shoppingcart');
-var cart = JSON.parse(jsonCart);
-cart.push({
-  name:'',
-  quantity:0
-});
-jsonCart = JSON.stringify(cart);
-localStorage.setItem('shoppingcart', jsonCart);  
+  event.preventDefault();
+  console.log('i invoked');
+  console.log(event.target);
+  var cart;
+  if(!localStorage.getItem('shoppingcart')){
+    cart = [];
+    cart.push({
+     name: event.target.items.value,
+     quanitity: event.target.count.value 
+    });
+    localStorage.setItem('shoppingcart', JSON.stringify(cart));
+  }else{
+    var jsonCart = localStorage.getItem('shoppingcart');
+    console.log(jsonCart);
+    cart = JSON.parse(jsonCart);
+    cart.push({
+      name: event.target.items.value,
+      quantity: event.target.count.value
+    });
+    jsonCart = JSON.stringify(cart);
+    localStorage.setItem('shoppingcart', jsonCart);  
+  }
+
 }
 
 
 
-function parse(text){
-
+function bootStrapApplication(){
+var submitForm = document.getElementById('submitform');
+submitForm.addEventListener('submit', addToCart);
+console.log('check if it works');
 }
+bootStrapApplication();
+
 
 
 
