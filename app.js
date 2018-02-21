6ff7288e2177e31bb6410f50c3ae0d91b9bf3d49
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-02-20 19:00:52 -0800

    solving problems

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 'use strict';
 
+var dropDown = document.getElementById('dropDown');
+
+var allpictures = [];
+
+
+function Pictures(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  allpictures.push(this);
+}
+
+new Pictures('Bag', 'img/bag.jpg');
+new Pictures('Banana Slicer', 'img/banana.jpg');
+new Pictures('Bathroom', 'img/bathroom.jpg');
+new Pictures('Boots', 'img/boots.jpg');
+new Pictures('Breakfast', 'img/breakfast.jpg');
+new Pictures('Meatball Gum', 'img/bubblegum.jpg');
+new Pictures('Chair', 'img/chair.jpg');
+new Pictures('Cthulhu', 'img/cthulhu.jpg');
+new Pictures('Dog-Duck', 'img/dog-duck.jpg');
+new Pictures('Dragon Meat', 'img/dragon.jpg');
+new Pictures('Pen', 'img/pen.jpg');
+new Pictures('Pet Sweeper', 'img/pet-sweep.jpg');
+new Pictures('Tauntaun', 'img/tauntaun.jpg');
+new Pictures('Unicorn Meat', 'img/unicorn.jpg');
+new Pictures('Usb', 'img/usb.gif');
+new Pictures('Water Can', 'img/water-can.jpg');
+new Pictures('Wine Glass', 'img/wine-glass.jpg');
+
 //local storage
 function addToCart(event){
   event.preventDefault();
@@ -9,8 +38,8 @@ function addToCart(event){
   if(!localStorage.getItem('shoppingcart')){
     cart = [];
     cart.push({
-     name: event.target.items.value,
-     quanitity: event.target.count.value 
+      name: event.target.items.value,
+      quanitity: event.target.count.value
     });
     localStorage.setItem('shoppingcart', JSON.stringify(cart));
   }else{
@@ -22,19 +51,29 @@ function addToCart(event){
       quantity: event.target.count.value
     });
     jsonCart = JSON.stringify(cart);
-    localStorage.setItem('shoppingcart', jsonCart);  
+    localStorage.setItem('shoppingcart', jsonCart);
   }
 
 }
 
-
-
 function bootStrapApplication(){
-var submitForm = document.getElementById('submitform');
-submitForm.addEventListener('submit', addToCart);
-console.log('check if it works');
+  var submitForm = document.getElementById('submitform');
+  submitForm.addEventListener('submit', addToCart);
+  console.log('check if it works');
 }
 bootStrapApplication();
+
+function dropDownList() {
+  for (var i = 0; i < allpictures.length; i++) {
+    var shopItems = document.createElement('option');
+    shopItems.textContent = allpictures[i].name;
+    dropDown.appendChild(shopItems);
+  }
+}
+dropDownList();
+
+
+
 
 //need 17img
 //create function that gets local storage array JSON.parse
